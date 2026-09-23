a8162541d27528c7be290453402defbeac34de95
SUKUNA <vimleshkumar901559@gmail.com>
2026-09-23 11:30:39 +0530

    Implement MainActivity with Picture-in-Picture support

---
+++ app/src/main/java/com/streamx/app/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.streamx.app
+
+import android.app.PictureInPictureParams
+import android.content.res.Configuration
+import android.os.Build
+import android.os.Bundle
+import android.util.Rational
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.streamx.app.ui.AppNavGraph
+import com.streamx.app.viewmodel.StreamXViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+class MainActivity : ComponentActivity() {
+
+    private var isPipMode = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val app = application as StreamXApp
+        
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val viewModel: StreamXViewModel = viewModel(
+                        factory = StreamXViewModel.provideFactory(app.database)
+                    )
+                    AppNavGraph(viewModel = viewModel, isPipMode = isPipMode)
+                }
+            }
+        }
+    }
+
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean,
+        newConfig: Configuration
+    ) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        isPipMode = isInPictureInPictureMode
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val params = PictureInPictureParams.Builder()
+                .setAspectRatio(Rational(16, 9))
+                .build()
+            enterPictureInPictureMode(params)
+        }
+    }
+}
